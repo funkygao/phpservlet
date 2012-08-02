4a11cf3b47b156c5196614287a9ec85e6b42ac79
funky gao <funky.gao@gmail.com>
2012-08-02 20:48:39 +0800

    test case base class

---
+++ test/DHttp_TestBase.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * 所有单元测试的父类.
+ *
+ * End programmers just include this file once and extends KxTestCaseBase to
+ * write unit test cases.
+ *
+ * @category test
+ * @package
+ * @version $Id$
+ * @author gaopeng <gaopeng@corp.kaixin001.com>
+ *
+ * vim: set sw=4 ts=4 et:
+ */
+abstract class DHttp_TestBase extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var float
+     */
+    protected $_beginAt;
+
+    private function getMilliSeconds()
+    {
+        list($usec, $sec) = explode(' ', microtime());
+        return ((float)$usec + (float)$sec);
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->_beginAt = $this->getMilliSeconds();
+    }
+
+    /**
+     * 取得当前单元测试用例的用时.
+     *
+     * @return float
+     */
+    protected final function getElapsedMs()
+    {
+        return $this->getMilliSeconds() - $this->_beginAt;
+    }
+
+    /**
+     * 取得某个文件的最后N行内容.
+     *
+     * @param string $filename 绝对路径
+     * @param int $lines 取最后多少行，默认1
+     *
+     * @return string
+     */
+    protected function getLastLine($filename, $lines = 1)
+    {
+        return `tail -n $lines $filename`;
+    }
+
+    /**
+     * @param string $filename
+     *
+     * @return string
+     */
+    protected function getFixtureFile($filename)
+    {
+        return 'fixtures/' . $filename;
+    }
+
+
+}
